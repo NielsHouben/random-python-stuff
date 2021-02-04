38032e7cccc566f2d06131259bddf6ffe3f86696
Niels Houben <Niels Houben>
2021-02-04 19:08:57 +0100

    minecraft finsished (from tutourial)

--- Ursina tutourial/main.py
+++ Ursina tutourial/main.py
@@ -1,41 +1,101 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+app = Ursina()
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture  = load_texture('assets/dirt_block.png')
+sky_texture   = load_texture('assets/skybox.png')
+arm_texture   = load_texture('assets/arm_texture.png')
+
+block_pick = 1
+
+def update():
+  global block_pick
+  
+  if held_keys['left mouse'] or held_keys['right mouse']:
+    hand.active()
+  else:
+    hand.passive()
+  
+  if held_keys['r']:
+    player.x = 0
+    player.z = 0
+    player.y = 10
+  if held_keys['1']:
+    block_pick = 1
+  if held_keys['2']:
+    block_pick = 2
+  if held_keys['3']:
+    block_pick = 3
+  if held_keys['4']:
+    block_pick = 4
+
+
 class Voxel(Button):
-  def __init__(self, position = (0,0,0)):
+  def __init__(self, position = (0,0,0), texture = grass_texture):
     super().__init__(
       parent = scene,
       position = position,
-      model = 'cube',
-      originy = 0.5,
-      texture = 'white_cube',
+      model = 'assets/block',
+      origin_y = 0.5,
+      texture = texture,
       color = color.color(0,0,random.uniform(0.9,1)),
-      highlight_color = color.lime,
+      highlight_color = color.white,
+      scale = 0.5
       )
   def input(self, key):
     if self.hovered:
-      if key == "left mouse down":
-        voxel = Voxel(position = (self.position + mouse.normal))
       if key == "right mouse down":
+
+        if block_pick == 1: voxel = Voxel(position = (self.position + mouse.normal), texture = grass_texture)
+        if block_pick == 2: voxel = Voxel(position = (self.position + mouse.normal), texture = stone_texture)
+        if block_pick == 3: voxel = Voxel(position = (self.position + mouse.normal), texture = brick_texture)
+        if block_pick == 4: voxel = Voxel(position = (self.position + mouse.normal), texture = dirt_texture)   
+      if key == "left mouse down":
         destroy(self)
+        
+class Sky(Entity):
+  def __init__(self):
+    super().__init__(
+      parent = scene,
+      model = 'sphere',
+      texture = sky_texture,
+      scale = 150,
+      double_sided = True
+    )
     
+class Hand(Entity):
+  def __init__(self):
+    super().__init__(
+      parent = camera.ui,
+      model = 'assets/arm',
+      texture = arm_texture,
+      scale = 0.2,
+      rotation = Vec3(150, -10, 0),
+      position = Vec2(0.4, -0.6)
+    ) 
+  def active(self):
+    self.position = Vec2(0.3, -0.5)
+  def passive(self):
+    self.position = Vec2(0.4, -0.6)
     
 
     
-app = Ursina()
 
 for z in range(15):
   for x in range(15):
     voxel = Voxel(position = (x, 0, z))
     
-def update():
-  if held_keys['r']:
-    player.x = 0
-    player.z = 0
-    player.y = 10
+
     
 player = FirstPersonController()
 player.y = 1
+
+sky = Sky()
+
+hand = Hand()
 
 app.run()
 
